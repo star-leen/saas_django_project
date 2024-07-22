63550316ffcf80d24457be9c1b579031b953365b
Nwokeocha Paul-Sterling <paulstarrliing@yahoo.com>
2024-07-22 14:14:28 +0100

    Added DJANGO_SECRET_KEY to Docker files and also made and implemented the helper module and Commando app to download our static files

--- src/base/settings.py
+++ src/base/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My apps
+    'commando',
 ]
 
 MIDDLEWARE = [
@@ -136,6 +139,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_BASE_DIR = BASE_DIR / 'staticfiles'
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
 STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / 'vendors'
 
 # Source(s) for python manage.py collectstatic:
